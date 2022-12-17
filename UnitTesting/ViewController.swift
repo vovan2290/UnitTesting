b37eba768b967b14af47020189c03f4d7d99775e
Volodymyr <vovhavgtr@gmail.com>
2022-12-17 15:25:09 -0500

    Initial Commit

---
+++ UnitTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnitTesting
+//
+//  Created by Imac on 12/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
